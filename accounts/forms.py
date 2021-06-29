e13bf49d5227c8e8032fc2dbdca22a6fe58f0d47
souleymane <souleymanedialloo@yahoo.fr>
2021-06-30 01:43:58 +0200

    add update profile

--- accounts/forms.py
+++ accounts/forms.py
@@ -2,9 +2,24 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 
 from .models import CustomUser
+from .models import Profile
 
 
 class UserRegisterForm(UserCreationForm):
     class Meta:
         model = CustomUser
         fields = ["email", "username", "password1", "password2"]
+
+
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = CustomUser
+        fields = ["username", "email"]
+
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ["image", "description"]
--- accounts/urls.py
+++ accounts/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name="accounts/login.html"), name="login"),
     path('logout/', auth_views.LogoutView.as_view(), name="logout"),
     path('dashboard/', views.dashboard, name="dashboard"),
+    path('profile/update', views.profile_update, name="profile-update"),
 ]
